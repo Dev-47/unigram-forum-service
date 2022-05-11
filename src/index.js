746a785bd5ecc4921d52ec28aa8cc0b6d4e7eaad
Horlakz <horlakz@protonmail.com>
2022-05-11 08:53:35 +0100

    style(src/index): imported packages and added func to get access routes and log them to a file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // imports
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const logger = require("morgan");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
@@ -14,10 +17,17 @@ connectDB();
 // initialise express app
 const app = express();
 
+// write stream to log file
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
 // middlewares
 app.use(express.json());
 app.use(errorHandler);
 app.use(cors());
+app.use(logger("dev", { stream: accessLogStream }));
 
 // routes
 app.use("/api/v1", require("./routes/forumRoutes"));
